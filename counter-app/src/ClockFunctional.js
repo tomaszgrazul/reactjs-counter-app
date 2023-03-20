ad80ba8db9e2503291673f646ec4889bbc8c57e9
tomaszgrazul <tgra@wp.pl>
2023-03-20 19:38:09 +0100

    ok

---
+++ counter-app/src/ClockFunctional.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+import './ClockFunctional.css';
+
+const ClockFunctional = (props) => {
+
+    console.log('inicjalizacja');
+    const [date, setDate] = useState(new Date());
+
+    const tick = () => setDate(new Date());
+
+    useEffect(() => {
+        let timerID = setInterval(() => {tick(), 1000 });
+        console.log('montowanie i update');
+
+        return (() => {
+            clearInterval(timerID);
+            console.log('umontning');
+        });
+    }, [date]);
+
+    return (
+        <div className="clock">
+            <h4>Time: {date.toLocaleTimeString()} <span onClick={props.toggleClockMethod}>x</span></h4>
+        </div>
+    );
+}
+
+export default ClockFunctional;
---
+++ counter-app/src/Counter.js
@@ -0,0 +1,99 @@
+import React, {Component} from 'react';
+import './Counter.css';
+
+import Dispaly from './Display';
+import ButtonsPanel from './ButtonsPanel';
+// import Clock from './Clock';
+import ClockFunctional from './ClockFunctional';
+
+class Counter extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            counterValue: this.props.initValue,
+            showClock: true,
+        };
+
+        // this.changeValue = this.changeValue.bind(this);
+    }
+
+    // changeValue() { // ES5 method (no this context by default)
+
+    changeValue = (action) => {
+
+        this.setState((prevState, prevProps) => {
+
+            let currentCounterValue = prevState.counterValue;
+
+            if (action === 'add') {
+
+                currentCounterValue += 1;
+
+            } else if (action === 'reinit') {
+
+                currentCounterValue = prevProps.initValue;
+
+            } else {
+                currentCounterValue = 0;
+            }
+
+            return ({
+                counterValue: currentCounterValue
+            });
+        });
+    }
+
+    toggleClock = () => {
+        this.setState((prevState) => {
+            return ({
+                showClock: !prevState.showClock
+            });
+        })
+    }
+
+    render() {
+
+            let clockElement  = '';
+
+            if (this.state.showClock) {
+                // clockElement = <Clock toggleClockMethod={this.toggleClock}/>;
+                clockElement = <ClockFunctional toggleClockMethod={this.toggleClock}/>;
+
+            } else {
+                clockElement = <span onClick={this.toggleClock} className='show-clock'>show clock</span>;
+            };
+        // let randomNumber = Math.floor(Math.random()*(108-1+1)+1);
+
+        return (
+            <div className="counter">
+                Counter:
+                <Dispaly displayValue={this.state.counterValue}/>
+                <ButtonsPanel buttonMethod={this.changeValue}/> 
+                {/* changeValue bez () bo jest to callback */}
+                {clockElement}
+            </div>
+        );
+    }
+}
+
+
+export default Counter;
+
+
+// function Counter(props) {
+ 
+//     let randomNumber = Math.floor(Math.random() * (108 - 1 + 1) + 1);
+ 
+//     return (
+//         <div className="counter">
+//             Counter:
+//             <span className="value">
+//                 {props.initValue}
+//             </span>
+//         </div>
+//     );
+// }
+
+// export default Counter;
